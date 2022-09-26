aac4bfa4e39e4938b1c1f8201f6b1334eb91bd7d
Sarang-Pi <sp872@cornell.edu>
2022-09-26 17:08:18 -0400

    no change, removed unwanted lines

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -1,9 +1,12 @@
 import time
+import math
 import subprocess
 import digitalio
 import board
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
+from adafruit_rgb_display.rgb import color565
+
 
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
 cs_pin = digitalio.DigitalInOut(board.CE0)
@@ -60,12 +63,110 @@ backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+
+def time_convert(sec):
+  mins = sec // 60
+  sec = sec % 60
+  hours = mins // 60
+  mins = mins % 60
+  return hours,mins,sec #"Time Lapsed = {0}:{1}:{2}".format(int(hours),int(mins),sec))
+
+#global Vars
+start = time.time()
+Budget = 100
+Spent = 0
+
 while True:
     # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    #draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    if not buttonA.value and  not buttonB.value:
+        Budget = 100
+    
+    if buttonA.value and not buttonB.value:  # just button B pressed
+        
+        #disp.fill(color565(255, 255, 255))  # set the screen to white
+        #draw.rectangle((0, 0, width, height), outline=0, fill=0)
+        
+        #while(buttonA.value and not buttonB.value): #button A not pressed
+        #counter = time.time()
+        #h,m,s = time_convert(counter-start)
+ 
+        #draw.text((0, 30), "Timer: "+"H: "+str(h)+" M: "+str(m)+" S: "+str(int(s)),font=font, fill=(255, 0, 0))
+        
+        #Button B  reduced money by 5
+        #Budget -= 5
+        Spent += 5
+        #if Budget < 0:
+            #Budget = 0
+    
+    if not buttonA.value and buttonB.value:  # just button B pressed
+        #Button A  increased money by 5
+        Spent -= 5
+    
+    if buttonA.value and  buttonB.value: #if neither are pressed
+        
+        y = top
+        
+        # Draw a black filled box to clear the image.
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+        
+        curhour = int(time.strftime("%H"))
+        curmin = int(time.strftime("%M"))
+        cursec = int(time.strftime("%S"))
+        
+        #draw hour bar
+        line_0 = 'Hrs:'
+        draw.text((0, 0 + y),line_0, font=font, fill = (255, 0, 0))
+        line_0size_x = font.getsize(line_0)[0]
 
-    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
+        hbar_width = 24
+        
+        draw.rectangle((line_0size_x, 0 + y, 24 + 9*curhour, hbar_width + y), outline=1, fill=(255, 0, 0))
+        draw.text((line_0size_x+5, 0), str(curhour), font=font, fill = "#FFFFFF")
+        
+        y += hbar_width        
+        
+        
+        line2 = "Budget of the day : "+str(Budget)
+        draw.text((0, y + 10), line2, font=font, fill="#FFFFFF")
+        y += font.getsize(line2)[1]
+        
+        
+        Val_tosub_permin = 4.1
+        Total_valToSub = Val_tosub_permin*curhour
+        RemaingBudget = float(Budget) - Total_valToSub
+        
+        line3 = "Remaining Money : " + str(RemaingBudget)
+        draw.text((0, y + 10), line3, font=font, fill="#FFFFFF")
+        y += font.getsize(line3)[1]
+        
+        line4 = "Spent : " + str(Spent)
+        
+        if(Spent >= RemaingBudget):
+            draw.text((0, y + 10), line4, font=font, fill=(255,0,0))
+        else:
+            draw.text((0, y + 10), line4, font=font, fill=(0,255,0))
 
-    # Display image.
+        y += font.getsize(line4)[1]
+        
+        
+        steps = 60
+        rec_width = width/steps
+        
+        draw.rectangle((0,30+y, rec_width*cursec, 20+y), outline=1, fill=(255, 0, 0))
+
+        
     disp.image(image, rotation)
-    time.sleep(1)
+    time.sleep(0.05)
+
+
+
+
+
+
+
+
